e59a9c4274ce6ab6ada9381e51057350b4926ec0
Aaron D <t-alternative@live.com>
2019-01-23 23:42:51 -0700

    iterates thru td, prints tr's

--- src/main/java/com/adodell/App.java
+++ src/main/java/com/adodell/App.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -14,23 +15,52 @@ import java.io.IOException;
 public class App 
 {
     public static void main( String[] args ) {
+
+    	String myURL;
+
+    	try{
+    		myURL = args[0].toString();
+		} catch (Exception e) {
+			System.out.println("No URL argument was input, using default...");
+			myURL = "https://en.wikipedia.org/wiki/Historical_components_of_the_Dow_Jones_Industrial_Average";
+		}
+
+    	/*
 		String myHTML = "<html><title>does this work?</title><p>TEST!</p></html>";
 		System.out.println(StringEscapeUtils.escapeHtml(myHTML) + "\nare we live?");
+		*/
 
-		String myURL = "https://en.wikipedia.org/wiki/Historical_components_of_the_Dow_Jones_Industrial_Average";
 		try {
 			Document myDocumentObj = Jsoup.connect(myURL).get();
+			Elements myTables_elements = myDocumentObj.select("table");
 
-			for (Element row : myDocumentObj.select("table.wikitable th")){
-				// this doesnt work...
-				System.out.println(row.toString());
+			// <table> iterator
+			for (Element table_element : myTables_elements) {
+				int table_i = myTables_elements.indexOf(table_element);
+				System.out.println("\n" + table_i + "\n~~~~~~~~~~~~~\n\n");
+
+				// <tSection> iterator
+				for (Element tSection_element : table_element.select("thead,tbody,tfoot")) {
+					// <tr > iterator
+					for (Element tr_element : tSection_element.select("tr")) {
+
+						if (tSection_element.select("tr").indexOf(tr_element) < 3){
+							System.out.println(tr_element.toString());
+						}
+
+						// <td > iterator
+						for (Element td_element : tr_element.select("td")) {
+							//System.out.println(td_element.toString());
+						}
+					}
+				}
+				if (table_i == 5){
+					break;
+				}
 			}
-
-			//this works!
-			System.out.println(myDocumentObj.outerHtml().substring(0,50000));
-
-		} catch (IOException e) {
-			System.out.println("Jsoup connect FAILED :(");
+		} catch (Exception e) {
+			System.out.println("Jsoup connect FAILED :(\n\n");
+			e.printStackTrace();
 		}
 
 	}
