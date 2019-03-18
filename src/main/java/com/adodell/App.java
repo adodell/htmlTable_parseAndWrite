15f75ecd89f863fc422503f84bb677fae0ee9025
Aaron D <t-alternative@live.com>
2019-03-17 17:38:04 -0700

    added url and file catches

--- src/main/java/com/adodell/App.java
+++ src/main/java/com/adodell/App.java
@@ -1,10 +1,14 @@
 package com.adodell;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -14,82 +18,106 @@ import java.util.ArrayList;
 public class App 
 {
     public static void main( String[] args ) {
-    	String myURL;
-
+    	String my_arg;
+		
+    	// grabbing arg
     	try{
-    		myURL = args[0].toString();
-		} catch (Exception e) {
-			System.out.println("No URL argument was input, using default Wikipedia DJIA Components History page...");
-			myURL = "https://en.wikipedia.org/wiki/Historical_components_of_the_Dow_Jones_Industrial_Average";
+    		//my_arg = args[0].toString();
+			my_arg = "C:\\Users\\Aaron\\Documents\\2Programming\\java\\htmlTable_parseAndWrite\\tableExample.html";
+		} 
+    	// if no arg use dfault
+    	catch (IndexOutOfBoundsException e) {
+			System.out.println("No argument was input, using default Wikipedia DJIA Components History page...");
+			my_arg = "https://en.wikipedia.org/wiki/Historical_components_of_the_Dow_Jones_Industrial_Average";
 
 			// another good example "http://www.eaglemarineservices.com/tmsweb/unsecure/UNSVSLRPT.tms"
+			// problems with "https://en.wikipedia.org/wiki/Mike_Tyson", debug this!
 		}
 
-		try {
-			Document myDocumentObj = Jsoup.connect(myURL).get();
+		Document myDocumentObj;
+    	// checking if it is a URL
+		try{
+			URL my_url = new URL(my_arg);
 
-			// creating Tables Elements lst
-			Elements tables_elements = myDocumentObj.select("table");
+			// parse URL using JSOUP
+			myDocumentObj = Jsoup.connect(my_arg).get();
 
-			// <table> iterator
-			for (Element tmp_table_element : tables_elements) {
+		}
+		// if not URL, try to see if it is a path
+		catch (MalformedURLException e) {
+    		File my_file = new File(my_arg);
 
-				// print location of this tmp_table
-				int table_i = tables_elements.indexOf(tmp_table_element);
-				System.out.println("\n" + table_i + "\n~~~~~~~~~~~~~\n\n");
+    		// if the file does not exist, throw an error
+			if (!my_file.exists()){
+				throw new IllegalArgumentException("The argument path input: " + my_arg + " does not exist");
+			}
 
-				// creation of Table StrBuilder
-				StringBuilder tmp_tableStrBuilder = new StringBuilder();
+			// parse using JSOUP
+			myDocumentObj = Jsoup.parse(my_file.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
 
-				// creating list of TSection Elements
-				Elements tSection_elements = tmp_table_element.select("thead,tbody,tfoot");
+			throw new IllegalArgumentException("The argument url input: " + my_arg+ " does not exist");
+		}
 
-				// <tSection> iterator
-				for (Element tmp_tSection_element : tSection_elements) {
+		// creating Tables Elements lst
+		Elements tables_elements = myDocumentObj.select("table");
 
-					// creating list of TR (and also TH) Elements
-					Elements tr_elements = tmp_tSection_element.select("tr,th");
+		// <table> iterator
+		for (Element tmp_table_element : tables_elements) {
 
-					// how many TR elements in this TSection?
-					int tmp_tSection_trSizeInt = tr_elements.size();
+			// print location of this tmp_table
+			int table_i = tables_elements.indexOf(tmp_table_element);
+			System.out.println("\n" + table_i + "\n~~~~~~~~~~~~~\n\n");
 
-					// <tr > iterator
-					for (int tmp_tr_i = 0; tmp_tr_i < tmp_tSection_trSizeInt; tmp_tr_i++) {
-						Element tmp_tr_element = tr_elements.get(tmp_tr_i);
-						// getting the TD Elements
-						Elements td_elements = tmp_tr_element.select("td");
+			// creation of Table StrBuilder
+			StringBuilder tmp_tableStrBuilder = new StringBuilder();
 
-						// how many TD elements in this TRow?
-						int tmp_tr_tdSizeInt = td_elements.size();
+			// creating list of TSection Elements
+			Elements tSection_elements = tmp_table_element.select("thead,tbody,tfoot");
 
-						// <td > iterator
-						for (int tmp_td_i = 0; tmp_td_i < tmp_tr_tdSizeInt; tmp_td_i++) {
-							// pulling <td> element
-							Element tmp_td_element = td_elements.get(tmp_td_i);
+			// <tSection> iterator
+			for (Element tmp_tSection_element : tSection_elements) {
 
-							// if this is not the last TD, append with ","
-							if (tmp_td_i + 1 != tmp_tr_tdSizeInt) {
-								tmp_tableStrBuilder.append(tmp_td_element.text()).append(",");
-							}
-							// otherwise this is LastTD, append with "\n"
-							else {
-								tmp_tableStrBuilder.append(tmp_td_element.text()).append("\n");
-							}
+				// creating list of TR (and also TH) Elements
+				Elements tr_elements = tmp_tSection_element.select("tr,th");
+
+				// how many TR elements in this TSection?
+				int tmp_tSection_trSizeInt = tr_elements.size();
+
+				// <tr > iterator
+				for (int tmp_tr_i = 0; tmp_tr_i < tmp_tSection_trSizeInt; tmp_tr_i++) {
+					Element tmp_tr_element = tr_elements.get(tmp_tr_i);
+					// getting the TD Elements
+					Elements td_elements = tmp_tr_element.select("td");
+
+					// how many TD elements in this TRow?
+					int tmp_tr_tdSizeInt = td_elements.size();
+
+					// <td > iterator
+					for (int tmp_td_i = 0; tmp_td_i < tmp_tr_tdSizeInt; tmp_td_i++) {
+						// pulling <td> element
+						Element tmp_td_element = td_elements.get(tmp_td_i);
+
+						// if this is not the last TD, append with ","
+						if (tmp_td_i + 1 != tmp_tr_tdSizeInt) {
+							tmp_tableStrBuilder.append(tmp_td_element.text()).append(",");
+						}
+						// otherwise this is LastTD, append with "\n"
+						else {
+							tmp_tableStrBuilder.append(tmp_td_element.text()).append("\n");
 						}
 					}
+				}
 
-					//all of the TR and TDs have been iterated through
-					// now print out StrBuilder for table
-					System.out.println(tmp_tableStrBuilder.toString());
-				}
-				// stopping after first 3 tables
-				if (table_i > 3){
-					break;
-				}
+				//all of the TR and TDs have been iterated through
+				// now print out StrBuilder for table
+				System.out.println(tmp_tableStrBuilder.toString());
 			}
-		} catch (Exception e) {
-			System.out.println("Jsoup connect FAILED :(\n\n");
-			e.printStackTrace();
+			// stopping after first 3 tables
+			if (table_i > 3){
+				break;
+			}
 		}
 
 	}
